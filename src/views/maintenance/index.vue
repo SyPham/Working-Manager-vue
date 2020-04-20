288b552cbd74caff0c44c684cc1aff661705582e
henry-pham <phamtiensy8696@gmail.com>
2020-04-20 10:39:24 +0700

    update1.0.6

--- src/views/maintenance/index.vue
+++ src/views/maintenance/index.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="content">
-
-
-<article>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:sy.pham@shc.ssbshoes.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+    <article>
+      <h1>We&rsquo;ll be back soon!</h1>
+      <div>
+        <p>
+          Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment.
+          If you need to you can always
+          <a href="mailto:sy.pham@shc.ssbshoes.com?cc=peter.tran@shc.ssbshoes.com;leo.doan@shc.ssbshoes.com
+              &subject=Maintenance%20Mode-Work%20Management%20System"
+          >contact us</a>
+          otherwise we&rsquo;ll be back online shortly!
+        </p>
         <p>&mdash; The Team</p>
-    </div>
-</article>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -17,10 +22,29 @@ export default {};
 </script>
 
 <style scoped>
-  .content { text-align: center; padding: 150px; }
-  h1 { font-size: 50px!important; }
-  .content { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
+.content {
+  text-align: center;
+  padding: 150px;
+}
+h1 {
+  font-size: 50px !important;
+}
+.content {
+  font: 20px Helvetica, sans-serif;
+  color: #333;
+}
+article {
+  display: block;
+  text-align: left;
+  width: 650px;
+  margin: 0 auto;
+}
+a {
+  color: #dc8100;
+  text-decoration: none;
+}
+a:hover {
+  color: #333;
+  text-decoration: none;
+}
 </style>
